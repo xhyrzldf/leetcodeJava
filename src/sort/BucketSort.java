84d998bb4539301e8e07065f960af052327915e4
xhyrzldf <296796683@qq.com>
2017-09-16 23:33:59 +0800

    增加了桶排序

---
+++ src/sort/BucketSort.java
@@ -0,0 +1,45 @@
+package sort;
+
+import static utils.SortUtil.*;
+
+/**
+ * @Description
+ * @Author 李迪凡[xhyrzldf@foxmail.com]
+ * @Date 2017/9/16 22:17
+ */
+public class BucketSort {
+    public static void main(String[] args) {
+        int[] array = getRandomArray(1000000,10);
+        printSortTime(array,BucketSort::bucketSort, "桶排序");
+
+    }
+
+    /**
+     * 桶排序
+     *
+     * @param array
+     */
+    public static void bucketSort(int[] array) {
+        //获得数组中的最大值,确定桶的数量
+        int maxIndex = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > array[maxIndex]) {
+                maxIndex = i;
+            }
+        }
+        //构建出这样一个桶数组,长度为最大值+1
+        int[] bucketArray = new int[array[maxIndex] + 1];
+        //统计原数组,将统计到的数字放到桶数组里
+        for (int anArray : array) {
+            bucketArray[anArray]++;
+        }
+        //将桶数组里的数字放入原数组替换
+        for (int i = 0, j = 0; i < bucketArray.length; i++) {
+            while (bucketArray[i] > 0) {
+                array[j] = i;
+                j++;
+                bucketArray[i]--;
+            }
+        }
+    }
+}
