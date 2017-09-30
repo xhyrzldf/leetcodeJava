b73b0f6cfdc33d86d0f477800626dec41db34882
xhyrzldf <296796683@qq.com>
2017-09-30 22:28:12 +0800

    修正与更新了two sum

--- src/array/TwoSum.java
+++ src/array/TwoSum.java
@@ -1,5 +1,7 @@
 package array;
 
+import com.sun.istack.internal.NotNull;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,19 +20,31 @@ import java.util.Map;
  */
 @SuppressWarnings("unused")
 public class TwoSum {
-    public int[] twoSum(int[] A, int target) {
-        if (A == null) return new int[]{0, 0};
-        Map<Integer, Integer> map = new HashMap<>();// 答案MAP，用于存储答案，key为数值,value为在集合中下标
 
-        for (int i = 0; i < A.length; i++) {
-            Integer answer = map.get(target - A[i]);//寻找答案,这里的answer是答案的下标
-            if (answer == null) {
-                map.put(A[i], i);//如果没有找到对应的答案,那么就把这个数本身作为答案放入答案map，根据a+b=b+a,正在遍历的数即是请求也是答案
-            } else {
-                int answerIndex = map.get(answer);//如果找到答案了,从map中取出对应的答案下表,返回回去
-                return new int[]{answerIndex, i};
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null) return new int[]{-1, -1};
+        Map<Integer, Integer> map = new HashMap<>();// 答案MAP，用于存储答案，key为数字,value为在数字集合中下标位置
+
+        for (int i = 0; i < nums.length; i++) {
+            Integer exceptNum = target - nums[i];//期望的答案
+            if (map.containsKey(exceptNum)) return new int[]{map.get(exceptNum), i}; //如果寻找到答案了,直接返回
+            else map.put(nums[i], i); //如果没有找到对应的答案,那么就把这个数本身作为答案放入答案map，根据a+b=b+a,正在遍历的数即是请求也是答案
+        }
+        return new int[]{-1, -1};
+    }
+
+    public int[] twoSum01(int[] arr, int target) {
+
+        if (arr == null) return new int[]{-1, -1};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) return new int[]{i, j};
             }
         }
-        return new int[]{0, 0};
+
+        return new int[]{-1, -1};
     }
+
+
 }
