dce95e9c3c0c091628638c2df29614127e34140a
xhyrzldf <296796683@qq.com>
2017-10-07 18:31:01 +0800

    添加并查集的测试用例

---
+++ src/test/UnionFindTest.java
@@ -0,0 +1,38 @@
+package test;
+
+import domain.UnionFind;
+
+/**
+ * <p>Description : leetcodeJava
+ * <p>Author : Matrix [xhyrzldf@foxmail.com]
+ * <p>Date : 2017/10/7 18:28
+ */
+public class UnionFindTest {
+  public static void main(String[] args) {
+    testUF(1000000);
+  }
+
+    public static void testUF( int n ){
+
+        UnionFind uf = new UnionFind(n);
+
+        long startTime = System.currentTimeMillis();
+
+        // 进行n次操作, 每次随机选择两个元素进行合并操作
+        for( int i = 0 ; i < n ; i ++ ){
+            int a = (int)(Math.random()*n);
+            int b = (int)(Math.random()*n);
+            uf.unionElements(a,b);
+        }
+        // 再进行n次操作, 每次随机选择两个元素, 查询他们是否同属一个集合
+        for(int i = 0 ; i < n ; i ++ ){
+            int a = (int)(Math.random()*n);
+            int b = (int)(Math.random()*n);
+            uf.isConnected(a,b);
+        }
+        long endTime = System.currentTimeMillis();
+
+        // 打印输出对这2n个操作的耗时
+        System.out.println("UF4, " + 2*n + " ops, " + (endTime-startTime) + "ms");
+    }
+}
