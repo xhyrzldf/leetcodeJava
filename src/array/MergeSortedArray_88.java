b9170a7728e50ee74e688690bf917ab7ed753765
xhyrzldf <296796683@qq.com>
2017-10-10 18:31:57 +0800

    MergeSortedArray_88 注释

--- src/array/MergeSortedArray_88.java
+++ src/array/MergeSortedArray_88.java
@@ -1,7 +1,12 @@
 package array;
 
 /**
- * Description : leetcodeJava
+ * Description : Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one
+ * sorted array.
+ *
+ * <p>Note: You may assume that nums1 has enough space (size that is greater or equal to m + n) to
+ * hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m
+ * and n respectively.
  *
  * <p>Author : Matrix [xhyrzldf@foxmail.com]
  *
@@ -10,6 +15,7 @@ package array;
 public class MergeSortedArray_88 {
   /**
    * 传统的归并排序开辟一个m空间的数组从前往后
+   *
    * @param nums1
    * @param m
    * @param nums2
@@ -31,23 +37,20 @@ public class MergeSortedArray_88 {
 
   /**
    * 反向归并,这样可以利用nums1数组多出来的空间
+   *
    * @param nums1
    * @param m
    * @param nums2
    * @param n
    */
   public void merge01(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1, j = n - 1, k = nums1.length - 1;
-        while(i >= 0 && j >= 0)
-        {
-            if(nums1[i] > nums2[j])
-                nums1[k--] = nums1[i--];
-            else
-                nums1[k--] = nums2[j--];
-        }
-        while(j >= 0)
-        {
-            nums1[k--] = nums2[j--];
-        }
+    int i = m - 1, j = n - 1, k = nums1.length - 1;
+    while (i >= 0 && j >= 0) {
+      if (nums1[i] > nums2[j]) nums1[k--] = nums1[i--];
+      else nums1[k--] = nums2[j--];
     }
+    while (j >= 0) {
+      nums1[k--] = nums2[j--];
+    }
+  }
 }
