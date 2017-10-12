b59573ce15dadff79c1b5d9497ad0f573325b61b
xhyrzldf <296796683@qq.com>
2017-10-13 04:56:11 +0800

    update string template practise1

--- src/string/LongestSubstringWithAtMostTwoDistinctCharacters_159.java
+++ src/string/LongestSubstringWithAtMostTwoDistinctCharacters_159.java
@@ -17,21 +17,18 @@ public class LongestSubstringWithAtMostTwoDistinctCharacters_159 {
     }
 
     public static int lengthOfLongestSubstringTwoDistinct(String s) {
-        int[] map = new int[128];
-        int counter = 0, begin = 0, end = 0, d = 0;
+        //0.初始化元素 [begin,end),本体是求最大值,counter的定义是滑动数组中,不重复的元素的个数
+        int begin = 0, end = 0, counter = 0, d = 0;
+        int[] freq = new int[128];
+        //1.确定counter的变化情况
+        //这里是如果碰到第一次出现的元素,也就是频率 ==0 ,那么counter++
         while (end < s.length()) {
-            if (map[s.charAt(end++)]++ == 0)  counter++;//end 每次碰到新元素 counter就+1
-
-            //下面这个循环使得子串再度满足题目条件
-            while (counter > 2) {//当counter > 2的时候,意味着此时子串里有三个不一样的元素,counter 应该是要<= 2的,因为题目中要求最多出现2个不重复的元素
-                if (map[s.charAt(begin)] == 1) {//只有某一个元素被彻底删除完了,counter才--,因为counter记录的是里面的元素种类
-                    counter--;
-                }
-                map[s.charAt(begin)]--;
-                begin++;
-            }
-
-            //最大值就在while循环外面更新
+            if (freq[s.charAt(end++)]++ == 0) counter++;
+            //2.确定counter达到目标值之后的操作以及修正情况
+            //在这里的条件是最多2个不重复的元素,也就是说当counter > 2就要进行修正,因为是最大值,所以是先修正再记录
+            //counter的修正条件特征是 当某一个元素彻底被删除了,counter才变化
+            while (counter > 2) if (freq[s.charAt(begin++)]-- == 1) counter--;
+            //3.确定返回值,因为滑动数组为[begin,end),所以长度是end-begin
             d = Math.max(d, end - begin);
         }
         return d;
