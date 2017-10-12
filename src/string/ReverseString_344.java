14cef1d103792a45708b070b901e9036f70dd124
xhyrzldf <296796683@qq.com>
2017-10-13 05:08:13 +0800

    update 344 translate

--- src/string/ReverseString_344.java
+++ src/string/ReverseString_344.java
@@ -5,6 +5,10 @@ package string;
  *
  * <p>Example: Given s = "hello", return "olleh" . "good" , return "doog"
  *
+ * <p>描述:编写一个函数，它以字符串作为输入，并将字符串返回。
+ *
+ * <p>示例:给定s = " hello "，返回" olleh "。“good”，返回“doog”
+ *
  * <p>Author : Matrix [xhyrzldf@foxmail.com]
  *
  * <p>Date : 2017/10/10 23:43
@@ -16,7 +20,6 @@ public class ReverseString_344 {
     char[] chars = s.toCharArray();
     int i = 0;
     int j = chars.length - 1;
-
     while (i < j) {
       chars[i] ^= chars[j];
       chars[j] ^= chars[i];
