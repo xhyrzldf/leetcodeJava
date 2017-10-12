c856d5c6280cd204c8976f8c1e9edb68bdc9ddb3
xhyrzldf <296796683@qq.com>
2017-10-13 05:28:21 +0800

    update 03 translate

--- src/string/LongestSubstringWithoutRepeatingCharacters_03.java
+++ src/string/LongestSubstringWithoutRepeatingCharacters_03.java
@@ -27,36 +27,9 @@ package string;
  *
  * <p>Date : 2017/10/11 22:18
  */
+@SuppressWarnings("unused")
 public class LongestSubstringWithoutRepeatingCharacters_03 {
   public int lengthOfLongestSubstring(String s) {
-
-    int l = 0, r = -1, n = s.length(); // 滑动数组[l,r]
-    int[] freq = new int[128]; // 使用128位数组来存储每个字母的状态
-    int res = 0;
-
-    while (l < n) {
-      if (r + 1 < n && freq[s.charAt(r + 1)] == 0) freq[s.charAt(++r)]++;
-      else freq[s.charAt(l++)]--;
-      res = Math.max(res, r - l + 1);
-    }
-
-    return res;
-  }
-
-  public int lengthOfLongestSubstring01(String s) {
-    int[] freq = new int[128];
-    int res = 0, n = s.length();
-
-    for (int start = 0, i = 0; i < n; i++) {
-      start = Math.max(freq[s.charAt(i)], start);
-      res = Math.max(res, i - start + 1);
-      freq[s.charAt(i)] = i + 1;
-    }
-
-    return res;
-  }
-
-  public int lengthOfLongestSubstring0102(String s) {
     int[] freq = new int[128];
     int begin = 0, end = 0, counter = 0, d = 0;
     while (end < s.length()) {
@@ -72,5 +45,33 @@ public class LongestSubstringWithoutRepeatingCharacters_03 {
       d = Math.max(d, end - begin);
     }
     return d;
+  }
+
+  public int lengthOfLongestSubstring01(String s) {
+
+    int l = 0, r = -1, n = s.length(); // 滑动数组[l,r]
+    int[] freq = new int[128]; // 使用128位数组来存储每个字母的状态
+    int res = 0;
+
+    while (l < n) {
+      if (r + 1 < n && freq[s.charAt(r + 1)] == 0) freq[s.charAt(++r)]++;
+      else freq[s.charAt(l++)]--;
+      res = Math.max(res, r - l + 1);
+    }
+
+    return res;
+  }
+
+  public int lengthOfLongestSubstring02(String s) {
+    int[] freq = new int[128];
+    int res = 0, n = s.length();
+
+    for (int start = 0, i = 0; i < n; i++) {
+      start = Math.max(freq[s.charAt(i)], start);
+      res = Math.max(res, i - start + 1);
+      freq[s.charAt(i)] = i + 1;
+    }
+
+    return res;
   }
 }
