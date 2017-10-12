1ff2e8196c4897b223f1420c2fb0636039701074
xhyrzldf <296796683@qq.com>
2017-10-13 04:43:21 +0800

    update string template practise

--- src/string/FindAllAnagramsInAString_438.java
+++ src/string/FindAllAnagramsInAString_438.java
@@ -7,80 +7,69 @@ import java.util.List;
  * Description : Given a string s and a non-empty string p, find all the start indices of p's
  * anagrams in s. Strings consists of lowercase English letters only and the length of both strings
  * s and p will not be larger than 20,100. The order of output does not matter.
- *
+ * <p>
  * <p>Example 1: Input: s: "cbaebabacd" p: "abc"
- *
+ * <p>
  * <p>Output: [0, 6]
- *
+ * <p>
  * <p>Explanation: The substring with start index = 0 is "cba", which is an anagram of "abc". The
  * substring with start index = 6 is "bac", which is an anagram of "abc".
- *
+ * <p>
  * <p>Author : Matrix [xhyrzldf@foxmail.com]
- *
+ * <p>
  * <p>Date : 2017/10/11 23:25
  */
 public class FindAllAnagramsInAString_438 {
-  public static void main(String[] args) {
-    String s = "bceabc";
-    String p = "ac";
-    findAnagrams(s, p);
-  }
+    public static void main(String[] args) {
+        String s = "cbaebabacd";
+        String p = "abc";
 
-  public static List<Integer> findAnagrams(String s, String p) {
-    List<Integer> list = new ArrayList<>();
-    if (s == null || s.length() == 0 || p == null || p.length() == 0) return list;
-    int[] hash = new int[256]; // character hash
-    // record each character in p to hash
-    for (char c : p.toCharArray()) {
-      hash[c]++;
+        System.out.println(findAnagrams(s, p).toString());
     }
-    // two points, initialize count to p's length
-    int left = 0, right = 0, count = p.length();
-    while (right < s.length()) {
-      // move right everytime, if the character exists in p's hash, decrease the count
-      // current hash value >= 1 means the character is existing in p
-      if (hash[s.charAt(right)] >= 1) {
-        count--;
-      }
-      hash[s.charAt(right)]--;
-      right++;
 
-      // when the count is down to 0, means we found the right anagram
-      // then add window's left to result list
-      if (count == 0) {
-        list.add(left);
-      }
-      // if we find the window's size equals to p, then we have to move left (narrow the window) to
-      // find the new match window
-      // ++ to reset the hash because we kicked out the left
-      // only increase the count if the character is in p
-      // the count >= 0 indicate it was original in the hash, cuz it won't go below 0
-      if (right - left == p.length()) {
+    public static List<Integer> findAnagrams(String s, String p) {
 
-        if (hash[s.charAt(left)] >= 0) {
-          count++;
+        //0.初始化一些变量,subString为[begin,end）
+        List<Integer> list = new ArrayList<>();
+        //null和空的判断,简单的写一下
+        if (s == null || s.length() == 0 || p == null || p.length() == 0) return list;
+        int begin = 0 , end =0 , counter = p.length();
+        int[] freq = new int[128];
+        for (char c : p.toCharArray()) freq[c]++;
+        //1.确定计数器在什么条件下进行怎么样的变化
+        //这里是当碰到子串p中的元素的时候,计数器要-1
+        //而子串P中元素的频率初始化为1,所以这里条件应该是 > 0 , 不管怎么样, 碰到的元素频率都要-1
+        while (end < s.length()) {
+            if (freq[s.charAt(end++)]-- > 0) counter--;
+            //2.当计数器条件满足的时候,要进行怎么样的操作,然后通过修正计数器跳出这个while
+            //2-1,当counter == 0 的时候,意味着当前子串是符合条件的,加入list集合
+            //2-2,修正计数器,这里有点变化,这里不能直接在while里修正,因为要保证子串里所有的元素都要是p子串中的
+            //因此修正步骤要先判断,如果当前子串的长度等于了p的长度,那么就要进行修正了,因为counter==0的情况下
+            //有可能是列俗语abdc这种字符串,长度已经大于p了,但是counter也为0,所以这里的计数器修正要变化一下
+            //counter变化的条件就是begin kick掉的元素是属于p字符串的,特征是freq频率>=0 因为默认至少为1,所以-1了之后至少为0
+            if (counter == 0) list.add(begin);
+            if (end - begin == p.length()) if (freq[s.charAt(begin++)]++ >= 0 ) counter++;
         }
-        hash[s.charAt(left)]++;
-        left++;
-      }
-    }
-    return list;
-  }
-
-  public List<Integer> findAnagrams01(String s, String p) {
-    List<Integer> list = new ArrayList<>();
-    if (s == null || s.length() == 0 || p == null || p.length() == 0) return list;
-    int[] hash = new int[256];
-    for (char c : p.toCharArray()) hash[c]++;
-
-    // 滑动数组为arr[left,right],count为需要达到的数组长度
-    int left = 0, right = -1, count = p.length();
-    while (right + 1< s.length()) {
-      if (hash[s.charAt(++right)]-- >= 1) count++;
-      if (count == p.length()) list.add(left);
-      if (right - left + 1 == p.length() && hash[s.charAt(left++)]++ >= 0) count--;
+        //3.返回结果
+        return list;
     }
 
-    return list;
-  }
+
+
+    public List<Integer> findAnagrams01(String s, String p) {
+        List<Integer> list = new ArrayList<>();
+        if (s == null || s.length() == 0 || p == null || p.length() == 0) return list;
+        int[] hash = new int[256];
+        for (char c : p.toCharArray()) hash[c]++;
+
+        // 滑动数组为arr[left,right],count为需要达到的数组长度
+        int left = 0, right = -1, count = p.length();
+        while (right + 1 < s.length()) {
+            if (hash[s.charAt(++right)]-- >= 1) count++;
+            if (count == p.length()) list.add(left);
+            if (right - left + 1 == p.length() && hash[s.charAt(left++)]++ >= 0) count--;
+        }
+
+        return list;
+    }
 }
--- src/string/MinimumWindowSubstring_76.java
+++ src/string/MinimumWindowSubstring_76.java
@@ -22,16 +22,26 @@ public class MinimumWindowSubstring_76 {
     }
 
     public static String minWindow(String s, String t) {
-        int[] map = new int[128];
-        for (char c : t.toCharArray()) map[c]++;
+        int[] freq = new int[128];
+        for (char c : t.toCharArray()) freq[c]++;
         int counter = t.length(), begin = 0, end = 0, d = Integer.MAX_VALUE, head = 0;
         while (end < s.length()) {
-            if (map[s.charAt(end++)]-- > 0) counter--; //in t
+            //计数器默认为子串t的长度,初始化的时候,子串t中的元素频率默认为1,非t中的频率为0
+            //1.确定计数器什么时候怎样变化
+            //当遍历到子串t中的任意一个字母的时候,计数器要-1
+            //所以在这里当频率大于0或者说大于等于1的时候,证明遍历到了t中的字符,计数器要-1
+            if (freq[s.charAt(end++)]-- > 0) counter--; //in t
+            //2.计数器修正,当counter == 0 的时候证明这个子串已经符合条件了,子串为[begin,end),长度为end-begin
             while (counter == 0) { //valid
-                if (end - begin < d) d = end - (head = begin);
-                if (map[s.charAt(begin++)]++ == 0) counter++;  //make it invalid
+                if (end - begin < d) d = end - (head = begin);//此时记录下最小的d,并且记录此时的begin
+                //修正计数器,使其再度不平衡,以寻找后面的字符串
+                //计数器需要修正的条件是当begin kick掉的元素是子串t中的元素时,counter应该增加
+                //此时,[begin,end)这个子串中,t中的元素应该是0,非t元素应该是<0的,所以==0 意味着这个元素是t中的元素
+                //此时counter计数器应该-1
+                if (freq[s.charAt(begin++)]++ == 0) counter++;  //make it invalid
             }
         }
+        //最终根据d来确定,如果D没变,那么返回空,否则返回之前while里保存的那个字符串
         return d == Integer.MAX_VALUE ? "" : s.substring(head, head + d);
     }
 }
