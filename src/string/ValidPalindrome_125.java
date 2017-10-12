a67db2e92f200874322c541248323bed6d2e0388
xhyrzldf <296796683@qq.com>
2017-10-13 05:00:09 +0800

    update 125 translate

--- src/string/ValidPalindrome_125.java
+++ src/string/ValidPalindrome_125.java
@@ -12,6 +12,14 @@ package string;
  *
  * <p>For the purpose of this problem, we define empty string as valid palindrome.
  *
+ * <p>描述:给定一个字符串，确定它是否是一个回文，只考虑字母数字字符和并且忽略大小写的情况。
+ *
+ * <p>例如:“A man, a plan, a canal: Panama" is a palindrome.”是一个回文。“race a car”不是一个回文。
+ *
+ * <p>注意:你是否考虑过字符串可能是空的?
+ *
+ * <p>为了解决这个问题，我们将空字符串定义为有效的回文。
+ *
  * <p>Author : Matrix [xhyrzldf@foxmail.com]
  *
  * <p>Date : 2017/10/10 23:37
@@ -21,7 +29,7 @@ public class ValidPalindrome_125 {
     if (s.isEmpty()) {
       return true;
     }
-    //利用正则直接替换,虽然速度相比于直接用unicode码来找会有些慢,但是在可以允许的时间房范围内这样我觉得代码和时间都是很好的
+    // 利用正则直接替换,虽然速度相比于直接用unicode码来找会有些慢,但是在可以允许的时间房范围内这样我觉得代码和时间都是很好的
     s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
     int n = s.length();
     for (int i = 0; i < n / 2; i++) {
